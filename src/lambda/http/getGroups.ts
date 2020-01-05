86052617ab0ed13695f244515b416b8c1e5ae864
johan <stjernholm@msn.com>
2020-01-05 13:17:22 +0800

    Feature: add getImage function

--- src/lambda/http/getGroups.ts
+++ src/lambda/http/getGroups.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
+import * as AWS from 'aws-sdk'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
